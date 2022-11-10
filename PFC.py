1fe334a40cc187b0128eeb702554b6536b077f6d
Urukagall <117281579+Urukagall@users.noreply.github.com>
2022-11-10 19:34:46 +0100

    Add files via upload

---
+++ PFC.py
@@ -0,0 +1,26 @@
+#on admet une fonction random qui renvoie un nombre aléatoire entre un nombres x et un autre y
+#on admet une fonction input qui renvoie ce que le joueur rentre
+
+#Début
+
+#définir la fonction PFC
+    #initialise playerWin en booléen et sur False
+    #initialise iaWin en booléen et sur False
+    #initialise la variable ia avec la valeur retourner par l'éxecution de la fonction random qui prendra en argument x=0 et y=2
+    #initialise la variable joueur avec le retour de l'éxecution de la fonction input
+    #si la variable joueur est égale à 0,1 ou 2 qui correspond dans l'ordre a pierre, feuille et ciseaux
+        #alors la variable joueur est convertit en integer
+        #si la variable ennemie est égale a la variable
+            #alors retourne "egalter"
+        #ou sinon la variable ennemie est égale a la variable joueur plus 1 (l'ennemie gagne avec un pierre ou une feuille)
+            #alors retourne "l'ennemie gane"
+        #ou sinon la variable joueur est égale a la variable ennemie plus 1 (le joueur gagne avec un pierre ou une feuille)
+            #alors retourne "le joueur gane"
+        #ou sinon la variable ennemie est égale à 0 et la variable joueur est égale à 2 (l'ennemie gagne avec une pierre)
+            #alors retourne "l'ennemie gane"
+        #ou sinon la variable joueur est égale à 0 et la variable ennemie est égale à 2 (le joueur gagne avec une pierre)
+            #alors retourne "le joueur gane"
+
+#Fin
+
+#Execution de la fonction PFC
